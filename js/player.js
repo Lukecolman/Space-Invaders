ca2f90d6e2411c2017d33f7bb91c8c9deece5a03
Luke Colman <lucas.a.colman@gmail.com>
2022-09-20 20:25:17 -0300

    Dibujando el canvas + inicio de controles

--- js/player.js
+++ js/player.js
@@ -1,59 +1,80 @@
+// Empece esta seccion desde cero para poder ir ordenando mi codigo anterior y para resolver problemas que tuve en el camino (como por ejemplo no se dibujaba en el canvas)
+
 //interactuando con el DOM
-const canvas = document.getElementById("canvas");
-const layout = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-canvas.width = 1200;
-canvas.height = 800;
-
-//cargo una imagen provisoria para probar el canvas
-const background = new Image();
-background.src = "../images/spaceSpiderMan.jpg"
-
-function game() {
-    layout.drawImage(background, 0, 0, canvas.width, canvas.height);
-}
-
-//el setInterval me permite visualizar la imagen del canvas
-setInterval(game, 1000 / 60);
+// canvas.widht = innerWidth;
+// canvas.height = innerHeight;
 
 
 
-//construyo el jugador
+//construyo el jugadors
 class Player {
     constructor() {
         this.position = {
-            x: 200, 
-            y: 200
+            x: canvas.width / 2 - 50,
+            // cambiar más adelante
+            y: canvas.height - 150
+        }
+        
+        this.velocity = {
+            x: 0,
         }
 
-        this.velocity = {
-            x: 0
-            
-        }
-        //this.img = 
-        this.width = 800;
-        this.height = 800;
+        this.width = 100;
+        this.height = 100;
     }
 
-    dibujo() {
-        layout.fillStyle = 'red';
-        layout.fillRect (this.position.x, this.position.y, this.with, this.height);
+// lo dibujo en el canvas
+    draw() {
+        ctx.fillStyle = "blue"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }   
-
 }
-console.log("averga")
-
 
 
 const player = new Player()
-player.dibujo()
+player.draw()
 
+// uso esta funcion para poder dibujar el fondo y para que se pueda volver a dibujar el personaje por arriba
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.fillStyle = "red"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+}
 
+animate()
 
 //EVENTOS
 
 let pizarra = document.querySelector(".avisoTecla")
 
-window.addEventListener("keydown", function (event) {console.log(event)
+window.addEventListener("keydown", function (event) {
+    console.log(event)
     pizarra.innerHTML = event.key + " - " +event.keyCode;
 },false);
+
+
+// Agrego un nuevo event listener para el movimiento del personaje + arrow function
+addEventListener("keydown", ({key}) => {
+    // console.log(key + " test fase 1")
+    switch (key) {
+        case "a":
+            console.log("funciona a");
+            break;
+        case "d":
+            console.log("funciona d");
+        break;
+        case "ArrowLeft":
+            console.log("funciona <-")
+        break;
+        case "ArrowRight":
+            console.log("funciona ->")
+        break;
+    }
+});
+
+
+// se que los colores estan horribles pero es para poder guiarme
